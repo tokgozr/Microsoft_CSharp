4e34d1d8d27441e3f43f41a6c2c420085a9b951a
tokgozr <126255709+tokgozr@users.noreply.github.com>
2024-07-11 15:15:39 +0300

    Add files via upload

---
+++ RANDOM DICE.cs
@@ -0,0 +1,44 @@
+﻿
+       // RANDOM DICE
+        Random dice = new Random();
+
+        int roll1 = dice.Next(1, 7);
+        int roll2 = dice.Next(1, 7);
+        int roll3 = dice.Next(1, 7);
+
+        int total = roll1 + roll2 + roll3;
+
+        Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+        if ((roll1 == roll2) || (roll2 == roll3) || (roll3 == roll1))
+        {
+            if ((roll1 == roll2) && (roll2 == roll3))
+            {
+                Console.WriteLine("You rolled triples! +6 bonus to total!");
+                total += 6;
+                Console.WriteLine("Your total score is: "+total);
+            }
+            else
+            {Console.WriteLine("You rolled doubles! +2 bonus to total!");
+            total += 2;
+            Console.WriteLine("Your total score is: "+total);
+            }
+        }
+        if (total >= 18)
+        {
+            Console.WriteLine("You win a new car!");
+        }
+        else if (total >= 12)
+        {
+            Console.WriteLine("You win a new laptop!");
+        }
+        else if (total >= 8)
+        {
+            Console.WriteLine("You win a trip for two");
+        }
+        else
+        {
+            Console.WriteLine("You win a kitten");
+        }
+
+    
